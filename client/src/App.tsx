28edfb81d0983111e23b69f2a104e60765b2933f
Edilvando Pereira Eufrazio <edilvando@MacBook-Pro-de-Edilvando-2.local>
2026-04-06 14:33:11 -0400

    fix: github pages routing and static deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -14,13 +14,13 @@ import CodigoProtegido from "./pages/CodigoProtegido";
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/estados"} component={Estados} />
-      <Route path={"/mato-grosso"} component={MatoGrosso} />
-      <Route path={"/metodologia"} component={Metodologia} />
-      <Route path={"/fontes"} component={FontesDados} />
-      <Route path={"/codigo"} component={CodigoProtegido} />
-      <Route path={"/404"} component={NotFound} />
+      <Route path="/" component={Home} />
+      <Route path="/estados" component={Estados} />
+      <Route path="/mato-grosso" component={MatoGrosso} />
+      <Route path="/metodologia" component={Metodologia} />
+      <Route path="/fontes" component={FontesDados} />
+      <Route path="/codigo" component={CodigoProtegido} />
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -32,7 +32,9 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <WouterRouter base="/desmatamento-evitado">
+            <Router />
+          </WouterRouter>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
